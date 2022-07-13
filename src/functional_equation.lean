e21cb2c5e1e0547413b75c969ad04a761ae783bc
madvorak <dvorakmartinbridge@seznam.cz>
2022-07-13 10:27:55 +0200

    increasing function, linear or rationals, is linear on reals

--- src/functional_equation.lean
+++ src/functional_equation.lean
@@ -1,11 +1,148 @@
 import data.real.basic
 
+lemma well_known {f : ℝ → ℝ} :
+  ∀ x y : ℝ, (∀ ε : ℝ, ε > 0 → |y - f x| < ε) → f x = y :=
+begin
+  intros x y ass,
+  sorry,
+end
+
+lemma min__div {a b c : ℝ} (c_pos : c > 0) :
+  (min a b) / c = min (a / c) (b / c) :=
+begin
+  unfold min,
+  unfold min_default,
+  split_ifs with hab habc habc,
+  {
+    refl,
+  },
+  {
+    exfalso,
+    rw ← div_le_div_right c_pos at hab,
+    exact habc hab,
+  },
+  {
+    exfalso,
+    rw div_le_div_right c_pos at habc,
+    exact hab habc,
+  },
+  {
+    refl,
+  },
+end
+
 lemma lin_on_real {f : ℝ → ℝ}
   (increasing : ∀ x y : ℝ,  0 < x  →  x < y  →  f x < f y)
   (lin_on_rat : ∀ x : ℚ,  0 < x  →  f x = f 1 + (x - 1) * (f 2 - f 1)
   ) :           ∀ x : ℝ,  0 < x  →  f x = f 1 + (x - 1) * (f 2 - f 1)  :=
 begin
-  sorry,
+  have lt_f2_f1 : 0 < f 2 - f 1,
+  {
+    rw sub_pos,
+    exact increasing _ _ one_pos one_lt_two,
+  },
+  intros x x_pos,
+  have ex₁ : ∀ ε : ℝ, ε > 0 → ∃ x₁ : ℚ,
+    ↑x₁ < x  ∧  x - ↑x₁ < ε / (f 2 - f 1),
+  {
+    intros ε ε_pos,
+    have that_pos : ε / (f 2 - f 1) > 0,
+    {
+      exact div_pos ε_pos lt_f2_f1,
+    },
+    rcases exists_rat_btwn (sub_lt_self x that_pos) with ⟨x₁, below, above⟩,
+    use x₁,
+    split,
+    {
+      exact above,
+    },
+    {
+      rw sub_lt,
+      exact below,
+    },
+  },
+  have ex₂ : ∀ ε : ℝ, ε > 0 → ∃ x₂ : ℚ,
+    x < ↑x₂  ∧  ↑x₂ - x < ε / (f 2 - f 1),
+  {
+    intros ε ε_pos,
+    have that_pos : ε / (f 2 - f 1) > 0,
+    {
+      exact div_pos ε_pos lt_f2_f1,
+    },
+    rcases exists_rat_btwn (lt_add_of_pos_right x that_pos) with ⟨x₂, below, above⟩,
+    use x₂,
+    split,
+    {
+      exact below,
+    },
+    {
+      rw sub_lt_iff_lt_add',
+      exact above,
+    },
+  },
+  apply well_known,
+  intros ε ε_pos,
+  cases ex₁ (min ε (x/2)) sorry with x₁ hx₁, -- or should we multiply by `(f 2 - f 1)` ??
+  cases ex₂ ε ε_pos with x₂ hx₂,
+  cases hx₁ with x₁_lt_x dist₁,
+  cases hx₂ with x_lt_x₂ dist₂,
+  rw min__div lt_f2_f1 at dist₁,
+  have x₁_pos : 0 < (x₁ : ℝ),
+  {
+    rw lt_min_iff at dist₁,
+    sorry,
+  },
+  have fx₁ := lin_on_rat x₁ (by {
+    clear_except x₁_pos,
+    assumption_mod_cast,
+  }),
+  have fx₂ := lin_on_rat x₂ (by {
+    have x₂_pos := lt_trans x_pos x_lt_x₂,
+    clear_except x₂_pos,
+    assumption_mod_cast,
+  }),
+  have inc₁ := increasing ↑x₁ x x₁_pos x₁_lt_x,
+  have inc₂ := increasing x  ↑x₂ x_pos x_lt_x₂,
+  rw fx₁ at inc₁,
+  rw fx₂ at inc₂,
+  rw abs_lt,
+  split,
+  {
+    rw lt_sub,
+    apply lt_trans inc₂,
+    rw sub_neg_eq_add,
+    rw add_assoc,
+    rw add_lt_add_iff_left,
+    rw sub_mul,
+    rw sub_mul,
+    rw sub_add_comm,
+    rw add_comm,
+    rw ← add_sub_assoc,
+    rw sub_lt_sub_iff_right,
+    rw ← sub_lt_iff_lt_add',
+    rw ← sub_mul,
+    rw ← lt_div_iff lt_f2_f1,
+    exact dist₂,
+  },
+  {
+    have intermediate_ineq :
+      f 1 + (x - 1) * (f 2 - f 1) - f x <
+      f 1 + (x - 1) * (f 2 - f 1) - (f 1 + (↑x₁ - 1) * (f 2 - f 1)),
+    {
+      exact sub_lt_sub_left inc₁ (f 1 + (x - 1) * (f 2 - f 1)),
+    },
+    apply lt_trans intermediate_ineq,
+    rw sub_mul,
+    rw sub_mul,
+    rw add_sub_add_left_eq_sub,
+    rw sub_sub_sub_cancel_right,
+    rw ← sub_mul,
+    rw ← lt_div_iff lt_f2_f1,
+    {
+      clear_except dist₁,
+      finish,
+    },
+  },
 end
 
 theorem must_be_identity {f : ℝ → ℝ} (codomain : ∀ x : ℝ, f x > 0)
@@ -41,6 +178,9 @@ begin
       ∀ x : ℝ, x > 0 → f (x + δ) = f x + ε,
   {
     intros δ restrictions,
+    have delta_inv_pos : 0 < 1 / δ, exact one_div_pos.mpr restrictions.1,
+    have f1_inv_pos : 1 / f 1 > 0, exact one_div_pos.mpr (codomain 1),
+
     let B := (A + 1 / δ) / 2,
     let C := (A + 1 / δ) * (1 / δ) / ((1 / δ) - A),
     have delta_eq : δ = 1 / B - 1 / C,
@@ -52,9 +192,6 @@ begin
       have positiv : A + 1 / δ > 0,
       {
         change 1 / f 1 + 1 + 1 / δ > 0,
-        have summand₁ : 1 / f 1 > 0, exact one_div_pos.mpr (codomain 1),
-        have summand₂ : 1       > 0, exact one_pos,
-        have summand₃ : 1 / δ   > 0, exact one_div_pos.mpr restrictions.1,
         linarith,
       },
       calc 2 / (A + 1 / δ) - (1 / δ - A) / ((A + 1 / δ) * (1 / δ))
@@ -71,26 +208,44 @@ begin
     {
       change 1 / f 1 + 1 > 0,
       specialize codomain 1,
-      have inv_f1_pos : 1 / f 1 > 0, exact one_div_pos.mpr codomain,
       linarith,
+    },
+    have A_lt_delta_inv : A < 1 / δ,
+    {
+      exact (lt_one_div restrictions.1 A_pos).mp restrictions.2,
     },
     have B_gt_A : B > A,
     {
       change (A + 1 / δ) / 2 > A,
-      have enough : 1 / δ > A,
-      {
-        exact (lt_one_div restrictions.1 A_pos).mp restrictions.2,
-      },
       linarith,
     },
     have C_gt_B : C > B,
     {
       have is_pos := restrictions.1,
-      rw delta_eq at is_pos,
-      have almost : 1 / C < 1 / B, exact sub_pos.mp is_pos,
+      have inv_ineq : 1 / C < 1 / B,
+      {
+        rw delta_eq at is_pos,
+        exact sub_pos.mp is_pos,
+      },
       have B_pos : B > 0, exact gt_trans B_gt_A A_pos,
-      have C_pos : C > 0, sorry,
-      exact (one_div_lt_one_div C_pos B_pos).mp almost,
+      have C_pos : C > 0,
+      {
+        apply mul_pos,
+        apply mul_pos,
+        {
+          exact add_pos A_pos delta_inv_pos,
+        },
+        {
+          rw one_div_pos,
+          exact is_pos,
+        },
+        {
+          rw inv_pos,
+          rw sub_pos,
+          exact A_lt_delta_inv,
+        },
+      },
+      exact (one_div_lt_one_div C_pos B_pos).mp inv_ineq,
     },
     obtain ⟨y₁, preimage_B⟩ : ∃ y₁ : ℝ, f y₁ = B,
     {
@@ -100,7 +255,14 @@ begin
     {
       exact high_range C (gt_trans C_gt_B B_gt_A),
     },
-    have equ : ∀ x : ℝ, x > 0 → 1 / f y₁ + x + 1 = 1 / f y₂ + (x+δ) + 1, sorry,
+    have equ : ∀ x : ℝ, x > 0 → 1 / f y₁ + x + 1 = 1 / f y₂ + (x+δ) + 1,
+    {
+      intros x x_pos,
+      rw delta_eq,
+      rw preimage_B,
+      rw preimage_C,
+      ring,
+    },
     have henc : ∀ x : ℝ, x > 0 → f (f x + (y₁ + 1) / (f y₁)) = f (f (x+δ) + (y₂ + 1) / (f y₂)),
     {
       intros x x_pos,
@@ -111,7 +273,7 @@ begin
       rw preimage_C,
       ring,
     },
-    have byinj : ∀ x : ℝ, x > 0 → f x + (y₁ + 1) / (f y₁) = f (x+δ) + (y₂ + 1) / (f y₂),
+    have by_inj : ∀ x : ℝ, x > 0 → f x + (y₁ + 1) / (f y₁) = f (x+δ) + (y₂ + 1) / (f y₂),
     {
       intros x x_pos,
       exact inj_f (henc x x_pos),
@@ -138,19 +300,18 @@ begin
     calc x + ↑n * δ ≥ x : by finish
     ... > 0 : x_pos,
   },
-  have lin_on_rat : ∀ x : ℚ,  0 < x  →  f x = f 1 + (x - 1) * (f 2 - f 1),
+  have f_lin_on_rat : ∀ x : ℚ,  0 < x  →  f x = f 1 + (x - 1) * (f 2 - f 1),
   {
     intros x x_pos,
     sorry,
   },
-  have increasing_f :  ∀ x y : ℝ,  0 < x  →  x < y  →  f x < f y,
+  have f_increasing :  ∀ x y : ℝ,  0 < x  →  x < y  →  f x < f y,
   {
     sorry,
   },
-  have lin_on_real : ∀ x : ℝ,  0 < x  →  f x = f 1 + (x - 1) * (f 2 - f 1),
+  have f_lin_on_real : ∀ x : ℝ,  0 < x  →  f x = f 1 + (x - 1) * (f 2 - f 1),
   {
-    intros x x_pos,
-    sorry,
+    exact lin_on_real f_increasing f_lin_on_rat,
   },
 
   have degree_one : ∃ a b : ℝ, ∀ x : ℝ,  0 < x  →  f x = a * x + b,
@@ -158,9 +319,8 @@ begin
     use f 2 - f 1,
     use f 1 + f 1 - f 2,
     intros x x_pos,
-    rw lin_on_real x,
+    rw f_lin_on_real x x_pos,
     ring,
-    sorry,
   },
   rcases degree_one with ⟨a, b, hf⟩,
   have a_eq_1 : a = 1,
